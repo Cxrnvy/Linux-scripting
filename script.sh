4710e8a93843dba6197b38ff43e4e7b69172311d
Cexnvy <cearciniegasme@uide.edu.ec>
2025-12-04 13:20:30 +0000

    Mi segundo script.

--- script.sh
+++ script.sh
@@ -1,11 +1,13 @@
 #Crear listas y archivos
 echo "Hola Linux" > archivo.txt
-#echo
+#echo sirve para mostrar un mensaje en pantalla y con el ">" redirige el mensaje de echo a un archivo
+#Si el archivo no existe, lo crea y si ya existe, lo sobreescribe
 cat archivo.txt
-#
+#leer el contenido de un archivo y mostrarlo en la pantalla
 cp archivo.txt copia.txt
-#
+#crea un duplicado exacto del archivo. Este requiere 2 argumentos, el archivo origen y el archivo destino.
 ls -l
-#
+#entrega nombre de los archivos e información detallada organizada en columnas en formato largo por el "-l"
 rm archivo.txt
-
+#rm viene de remove. Elimina archivos o directorios de forma permanente. En este caso
+#borra el archivo original "archivo.txt" y solo queda el archivo copia.
---
+++ script2.sh
@@ -0,0 +1,8 @@
+uname -a
+#Muestra información técnica del sistema operativo y el kernel
+lscpu
+#
+free -h
+#
+df -h
+#
